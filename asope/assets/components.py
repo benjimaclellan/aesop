Fixed gradient descent
Fixed final optimization packing/unpacking

asope/assets/components.py
@@ -209,7 +209,7 @@ class AWG(Component):
     def simulate(self, env, At, visualize=False):   
         # extract attributes, first index is the number of steps - which affects the other attributes
         nlevels = self.at[0] + 1
-        
+
         # phase to put on each step
         phasevalues = [0] + self.at[1:]
         
@@ -358,6 +358,12 @@ class PowerSplitter(Component):
         
     def simulate(self, env, At_in, num_outputs, visualize=False):        
         # ensure there is maximum 2 inputs/outputs (for now)
+
+        # this is a hacky fix -- look into a better solution later        
+        if At_in.ndim == 1:
+            At_in = np.atleast_2d(At_in).T
+
+        
         num_inputs = At_in.shape[1]
         assert num_inputs <= 2
         assert num_outputs <= 2

asope/assets/environment.py
@@ -106,9 +106,9 @@ class PulseEnvironment(object):
         # one value to optimize is the peak power
 #        fitness2 = np.max(P(At))
         PAt = P(At)
-#        fitness2 = np.max(PAt) #* np.std((PAt))
-        dPAt = np.diff(PAt)
-        fitness2 = np.max(PAt) * ((dPAt[:-1] * dPAt[1:]) < 0).sum() / self.N       
+        fitness2 = 1-np.power( np.max(PAt) - p/q, 2) #
+#        dPAt = np.diff(PAt)
+#        fitness2 = np.max(PAt) * ((dPAt[:-1] * dPAt[1:]) < 0).sum() / self.N       
         
         # find the harmonic frequencies in the RF domain
         peakinds = peakutils.indexes(RFSpectrum(At, self.dt))
@@ -124,7 +124,11 @@ class PulseEnvironment(object):
             fitness1 = 0 #-99999999
         # if there are peaks in the RF spectrum, define the fitness function here, based on how far we are from the target
         else: 
-            fitness1 = 1 - np.power( peakf_distTarget - peakf_dist, 2)
+            X = np.power( peakf_distTarget - peakf_dist, 2)
+#            A = 0.01
+            fitness1 = max([0,1 - X])
+#            fitness1 = 8*(1/(1+np.exp(-X/A)))*(1-1/(1+np.exp(-X/A)))
+
 
         return (fitness1, fitness2)
     

asope/optimization/geneticalgorithminner.py
@@ -99,7 +99,7 @@ def SEL_Inner(individuals, k):
 Fitness function for Inner GA
 """
 def FIT_Inner(individual, env, experiment):
-
+        
     experiment.setattributes(individual)
     experiment.simulate(env)
     
@@ -107,7 +107,8 @@ def FIT_Inner(individual, env, experiment):
     At = experiment.nodes[measurement_node]['output'].reshape(env.N)
     fitness = env.fitness(At)
 
-    return fitness[0]*fitness[1],
+#    return np.sqrt(np.sum(np.power(fitness,2))) * (1-env.component_losses/100),
+    return fitness[0],fitness[1],
 
 
 ## --------------------------------------------------------------------
@@ -145,8 +146,8 @@ def inner_geneticalgorithm(gap, env, experiment):
     stats = tools.Statistics(lambda ind: ind.fitness.values)
     
     stats.register("avg", np.mean)
-#    stats.register("std", np.std)
-#    stats.register("min", np.min)
+    stats.register("std", np.std)
+    stats.register("min", np.min)
     stats.register("max", np.max)
 
     # setup variables early, in case of an early termination

asope/optimization/gradientdescent.py
@@ -1,5 +1,7 @@
 import scipy.optimize as opt
 from copy import copy
+import numpy as np
+
 
 """
 Wrapping function for final fine-tuning of the GA HOF individuals
@@ -7,7 +9,7 @@ Wrapping function for final fine-tuning of the GA HOF individuals
 def finetune_individual(ind, env, experiment):
     (optlist, idx, nodelist) = pack_opt(ind, experiment)
     
-    optres = opt.minimize(final_opt, optlist, args=(ind, idx, nodelist, env, experiment), method='Nelder-Mead', options={'maxiter':2000})    
+    optres = opt.minimize(final_opt, optlist, args=(ind, idx, nodelist, env, experiment), method='Nelder-Mead', options={'maxiter':3000})    
     
     ind = unpack_opt(ind, experiment, optres.x.tolist(), idx, nodelist)
     return ind
@@ -17,6 +19,8 @@ Using standard packages to slightly tweak the best of HOF to optimal individual
 """
 def final_opt(optlist, ind, idx, keylist, env, experiment):    
     ind = unpack_opt(ind, experiment, optlist, idx, keylist)
+#    print('final_opt', ind)
+    
     measurement_node = experiment.measurement_nodes[0]
     
     experiment.setattributes(ind)
@@ -25,7 +29,9 @@ def final_opt(optlist, ind, idx, keylist, env, experiment):
     At = experiment.nodes[measurement_node]['output'].reshape(env.N)
     fitness = env.fitness(At)
     
-    return -fitness[0] * fitness[1] #minus sign is because we want to minimize
+    return -fitness[0]*0.05- fitness[1]*0.95
+#    return np.sqrt(np.sum(np.power(fitness,2))) #* (1-env.component_losses/100)
+#    return -fitness[1] #minus sign is because we want to minimize
 
 """
 Unpacks the list of parameters from the structure used in the GA to the GD
@@ -40,26 +46,26 @@ def pack_opt(ind, experiment):
             if (experiment.nodes[node]['info'].FINETUNE_SKIP != None) and i in (experiment.nodes[node]['info'].FINETUNE_SKIP):
                 continue
             optlist.append(at_i)
-            nodelist.append(node)
+        
             cnt += 1
+        nodelist.append(node)
         a[1] = cnt
         idx.append(copy(a))
+
     return optlist, idx, nodelist
 
 """
 Unpacks the list of parameters from the structure used in GD to the GA and simulation
 """
 def unpack_opt(ind, experiment, optlist, idx, nodelist):
+    cnt = 0
     for i, ind_pair in enumerate(idx, 0):
-        start = ind_pair[0]
-        end = ind_pair[1]# + 1
         node = nodelist[i]
-        cnt = 0
-        for j in range(start, end):
-            if (experiment.nodes[node]['info'].FINETUNE_SKIP != None) and (i-start) in (experiment.nodes[node]['info'].FINETUNE_SKIP):
-                continue
+        for j in range(0, len(ind[node])):
+            if (experiment.nodes[node]['info'].FINETUNE_SKIP != None) and (j) in (experiment.nodes[node]['info'].FINETUNE_SKIP):
+                pass
             else:
-                ind[node][cnt-start] = optlist[cnt]
+                ind[node][j] = optlist[cnt]
                 cnt += 1
     return ind
         