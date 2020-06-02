67c31ec1b9e60b0373ccf16984a578c8ad2f1033
Julie Belleville <julie@DESKTOP-MRCH6LM.localdomain>
2020-06-02 10:00:25 -0700

    refactored max eye to be differentiable, with the power of ellipses

--- problems/example/evaluator_subclasses/max_eye_evaluator.py
+++ problems/example/evaluator_subclasses/max_eye_evaluator.py
@@ -1,7 +1,10 @@
 import autograd.numpy as np
 from autograd import grad
+import matplotlib.patches as patches
+import matplotlib.pyplot as plt
 
 from ..evaluator import Evaluator
+from ..assets.functions import power_
 
 """
 Author: Julie Belleville
@@ -12,7 +15,7 @@ TODO: refactor into being differentiable under autograd (rn, using filters which
 
 class MaxEyeEvaluator(Evaluator):
     """
-    Given a certain hexagon shape (acting as a mask), find the largest hexagon
+    Given a certain elliptical shape (acting as a mask), find the largest ellipse of that shape
     which will fit in the inside of the eye diagram of the signal (without touching any
     datapoints)
 
@@ -22,7 +25,7 @@ class MaxEyeEvaluator(Evaluator):
 # ----------------------------- Public API ------------------------------------
 
     def __init__(self, propagator, bit_sequence, bit_width, eye_mask=None,
-                 mock_graph_for_testing=False):
+                 mock_graph_for_testing=False, graphical_testing=False):
         """
         Initialises object with an values needed for evaluation
 
@@ -50,17 +53,17 @@ class MaxEyeEvaluator(Evaluator):
         if (eye_mask is None): # use default mask
             self.mask = MaxEyeEvaluator.get_eye_diagram_mask(bit_width * propagator.dt)
 
+        self._graphical_testing = graphical_testing
+
     @staticmethod
-    def get_eye_diagram_mask(relative_width=0.5, relative_height=0.3, centre_to_sides_ratio=0.4, centered=False):
+    def get_eye_diagram_mask(semi_major=0.3, semi_minor=0.15):
         """
-        Returns a dictionary representing the eye_diagram mask. Mask is a hexagon with flat top and bottom
+        Returns a dictionary representing the eye_diagram mask. Mask is an ellipse
         The dictionary contains (key, val) pairs:
-            1. ('relative_width', hexagon width / bit width)
-            2. ('relative_height', hexagon height) (height normalised to 1)
-            3. ('centre_to_Sides_ratio', flat top length / total hexagon width)
-        :param relative_width : the total width of the eye diagram
-        :param relative_height : the total height of the eye diagram
-        :param centre_to_sides_ratio : the ratio of the centre potion (flat top width) to total width
+            1. ('semi_major', semi_major)
+            2. ('semi_minor', semi_minor) (height normalised to 1)
+        :param semi_major : semi-major axis length (length is relative to semi-minor, only ratio matters)
+        :param semi_minor : semi-minor axis length
         """
         return {
                 'relative_width': width_ratio,
@@ -70,7 +73,11 @@ class MaxEyeEvaluator(Evaluator):
 
     def evaluate_graph(self, graph, eval_node=None):
         """
-        TODO: docstring
+        Returns the inverse of the height of the largest ellipse which can
+        fit inside the eye diagram of the data
+
+        :param graph : the graph to evaluate
+        :param eval_node : node to evaluate (if None, defaults to the final node)
         """
         if (eval_node is None):
             eval_node = len(graph.nodes) - 1 # set to the last node, assume they've been added in order
@@ -80,12 +87,19 @@ class MaxEyeEvaluator(Evaluator):
         else:
             state = graph # mock is passed in as graph
 
-        return self._max_eye(state)
+        return self._max_eye_inverse(state)
 
 # --------------------------- Helper functions ---------------------------------
-    def _max_eye(self, state):
+    def _max_eye_inverse(self, state):
         """
-        TODO: docstring
+        Returns the inverse of the max area for our mask shape, such that
+        the mask still fits inside the (centered and normalised) eye diagram
+        Scaling coefficient scales major and minor axis:: x^2/(a^2) + y^2/(b^2) = s^2 where
+        a, b, are the relative major and minor axes, s is the scaling coefficient. Thus, the
+        area is A = pi * (a * s) * (b * s) = pi * a * b * s^2
+
+        :param state: the state to test
+        :return 1/<max scaling coefficient>
         """
         if (not self.mock_graph_for_testing):
             power = power_(state)
@@ -95,57 +109,27 @@ class MaxEyeEvaluator(Evaluator):
         normalized = power / np.max(power)
 
         # shift datapoints, such that our mask is centred at (0, 0)
-        
         generated = normalized - 0.5 # shift from [0, 1] to [-0.5, 0.5]
-        # normalise time to [-0.5, 0.5).
-        time_tile = np.linspace(-0.5, 0.5, self._bit_width)
+        time_tile = np.linspace(-0.5, 0.5, self._bit_width) # normalise time to [-0.5, 0.5).
         time = np.tile(time_tile, (self._target.shape[0])) 
-            
-        # all points with angles in [theta, pi - theta] or [- pi + theta, -theta] from the horizontal restrict the 
-        # mask height vertically (that is, h_max < y_max in the range)
-        theta = np.arctan2((self.mask['relative_height']),
-                        (self.mask['relative_width'] * self.mask['centre_to_sides_ratio']))   
-            
-        #generate filter for all points which touch the top or bottom of the hexagon
-        datapoint_angles = np.arctan2(generated, time)
-        filter = np.bitwise_or(np.bitwise_and(theta < datapoint_angles, datapoint_angles < (np.pi - theta)),
-                            np.bitwise_and(-np.pi + theta < datapoint_angles, datapoint_angles < -theta))        
-        try:
-            candidate_max_height = 2 * abs(generated[filter]).min()
-        except ValueError: # occurs if no points are in the array, and thus there is no min
-            candidate_max_height = 1 # if there are no points in this region, no height bound
 
-        # find max height allowed due to "side" points. Absolute values used to reduce math to first quadrant math, from symmetry
-        inverse_filter = np.invert(filter)
-        side_y = abs(generated[inverse_filter])
-        side_x = abs(time[inverse_filter])
-        h = self._mask['relative_height']
-        w = self._mask['relative_width']
-        m = - h / (w * (1 - self._mask['centre_to_sides_ratio'])) # hexagon side slope
-        try:
-            candidate_max_height_2 = (2 * h / w * (side_x - side_y / m)).min()
-        except ValueError:
-            candidate_max_height = 1 
-
-        hex_height = min(candidate_max_height, candidate_max_height_2)
+        # compute max scaling for each point, and keep only the smallest
+        s_squared = (time**2 / self.mask['semi_major']**2 + generated**2 / self.mask['semi_minor']**2).min()
+        
         if (self._graphical_testing):
-            mask = CaseEvaluator.get_eye_diagram_mask(1,
-                                                    width_ratio=hex_height * w / h,
-                                                    height_ratio=hex_height,
-                                                    centre_to_sides_ratio=self._mask['centre_to_sides_ratio'],
-                                                    centered=True)
-                self._plot_eye_diagram(hex_height, filter, mask['path'], time, generated)
+            self._plot_eye_diagram(time, normalized, s_squared)
 
-            return hex_height
+        return 1 / (np.pi * self.mask['semi_major'] * self.mask['semi_minor'] * s_squared)
     
-    def _plot_eye_diagram(self, hex_height, filter, mask, x, y):
+    def _plot_eye_diagram(self, x, y, scaling):
         """
         TODO: docstring
         """
         fig, ax = plt.subplots()
-        patch = patches.PathPatch(mask, facecolor='orange', alpha=0.3, lw=2)
+        patch = patches.Ellipse((0, 0), 2 * self.mask['semi_major'] * np.sqrt(scaling),
+                                2 * self.mask['semi_minor'] * np.sqrt(scaling),
+                                facecolor='orange', alpha=0.3, lw=2)
         ax.add_patch(patch)
-        ax.scatter(x[filter], y[filter], color='r')
-        ax.scatter(x[np.invert(filter)], y[np.invert(filter)], color='b')
-        plt.title("Max hexagon of height {}".format(hex_height))
+        ax.scatter(x, y, color='r')
+        plt.title("Max mask (elliptical)")
         plt.show()
