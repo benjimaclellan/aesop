0c9c3dcb538aad264cd32cf65b4c42344e81dbc0
Julie Belleville <julie@DESKTOP-MRCH6LM.localdomain>
2020-07-20 13:53:38 -0700

    completed prelim AdditiveNoise class, testing begun

--- problems/example/assets/additive_noise.py
+++ problems/example/assets/additive_noise.py
@@ -5,13 +5,8 @@ from .functions import fft_, power_, psd_
 
 """
 TODO: Check with Ben or Piotr
-- Does it make sense to have the noise type be either relative or absolute? I think so
-    - Generally higher power = more noise
-    - But there could be an environmental source or something which would not be dependent of power
 - Do we want the noise param to match the matlab script (i.e. be in dB) or to be the ratio (am leaning to the Matlab bc the scaling might be more sense)
-- OK so technically specifying the sample_num here is a problem bc it restricts the propagator side. SO THIS CANNOT STAY.
-    That said, regenerating all this info at each function call is extra
-    Think about a neat solution to this design flaw
+TODO: update functions to take propagator (not just vector) 1. Update functions 2. Update tests 3. change the frequency representation to be normalized smh
 """
 
 
@@ -24,7 +19,7 @@ class AdditiveNoise():
 
     simulate_with_noise = True
 
-    def __init__(self, sample_num=2**14, distribution='gaussian', noise_param=1, noise_filter=None, noise_type='relative', noise_on=True, seed=None):
+    def __init__(self, distribution='gaussian', noise_param=1, noise_filter=None, noise_type='relative', noise_on=True, seed=None):
         """
         Creates an noise object with the described properties
 
@@ -36,94 +31,150 @@ class AdditiveNoise():
         :param noise_on: True if the noise of the object is to be applied, False otherwise
                          Note that the noise can also be turned off for the whole class (upon which this variable has no effect)
         :param seed: seed with which np.random can generate the pseudorandom noise vectors
+
+        :raises ValueError if (1) non-gaussian distribution is requested (2) someone has the audacity of asking for a filter I have yet to implement
+                              (3) noise_type is not 'relative' or 'absolute'
         """
-        self.sample_num = sample_num
-        
-        if (distribution != 'gaussian'):
-            raise ValueError(f"{distribution} distribution not supported. Only 'gaussian' is supported at this time")
+        self._sample_num = None # defined the first time that noise is added to the propagation
+    
         if (noise_filter is not None):
             raise ValueError('Noise filter options not yet implemented, object must be None')
-        if (noise_type != 'relative' and noise_type != 'absolute'):
-            raise ValueError('Noise type must be relative or absolute')
         
+        self._seed = seed # can't actually seed it now because we have no idea what random calls will come between initialization and the first call to add_noise_to_propagate
+    
         # keep log of noise sources for debugging, and in case of resampling
         self.noise_sources = []
 
         # create noise distribution
-        self.sample_num = sample_num
-        self.add_noise_source(distribution=distribution, noise_param=noise_param, noise_filter=noise_filter, noise_type=noise_type, seed=seed)
+        self._sample_num = None
+        self.add_noise_source(distribution=distribution, noise_param=noise_param, noise_filter=noise_filter, noise_type=noise_type)
 
         self.noise_on = noise_on
     
-    def add_noise_source(self, distribution='gaussian', noise_param=1, noise_filter=None, noise_type='relative', seed=None):
+    def add_noise_source(self, distribution='gaussian', noise_param=1, noise_filter=None, noise_type='relative'):
         """
         Adds a noise source to the AdditiveNoise object (all future calls to add_noise_to_propagation will also include this source)
+        WARNING: all noise sources should probably be added at once (and non-concurrently please, this is not threadsafe yet) so that seeding actually makes results predictable
+                 We will seed ONCE when initializing the noise sources, so if you have an external random call, might make it harder to track down the precise noise vectors 
 
         :param distribution: type of noise distribution. ONLY GAUSSIAN IS SUPPORTED AT THIS TIME
         :param noise_param: scaling parameter for noise (standard deviation for Gaussian)
         :param noise_filter: None for now
         :param noise_type: 'relative' or 'absolute'. If relative, noise is scaled
         :param seed: seed with which np.random can generate the pseudorandom noise vectors
+    
+        :raises ValueError if (1) non-gaussian distribution is requested (2) someone has the audacity of asking for a filter I have yet to implement
+                              (3) noise_type is not 'relative' or 'absolute'
         """
         if (distribution != 'gaussian'):
             raise ValueError(f"{distribution} distribution not supported. Only 'gaussian' is supported at this time")
-        if (seed is not None):
-            np.random.seed(seed)
+        
+        if (noise_type != 'relative' and noise_type != 'absolute'):
+            raise ValueError('Noise type must be relative or absolute')
+        
+        if (noise_filter is not None):
+            raise ValueError('Noise filter options not yet implemented, object must be None')
 
-        noise = np.random.normal(scale=1, size=(self.sample_num, 2)).view(dtype='complex').flatten()
-        noise_scaling = np.linalg.norm(power_(noise))**0.5
+        if (self._sample_num is not None):
+            noise = np.random.normal(scale=1, size=(self.sample_num, 2)).view(dtype='complex').flatten()
+            noise_power_norm = np.linalg.norm(power_(noise))**0.5
+        else:
+            noise = None
+            noise_power_norm = None
+
         source = {'distribution': distribution,
                   'noise_param': noise_param,
                   'filter' : noise_filter,
                   'noise_type': noise_type,
                   'noise_vector': noise,
-                  'noise_scaling': noise_scaling
+                  'noise_power_norm': noise_power_norm
                  }
         self.noise_sources.append(source)
     
-    def add_noise_to_propagation(self, state):
+    def add_noise_to_propagation(self, signal):
         """
         Adds noise to the input state, and returns the noisy state
         
-        :param state: the pre-noise state
+        :param signal: the pre-noise state
         :return: the state including noise
         """
+        if (self._sample_num is None):
+            self.sample_num = signal.shape[0]
+        elif (self.sample_num != signal.shape[0]):
+            raise ValueError(f'signal length is {signal.shape[0]} much match sample_num {self.sample_num}')
+
         if (not self.noise_on or not AdditiveNoise.simulate_with_noise):
-            return state
+            return signal
         
-        state_scaling = np.linalg.norm(power_(state))**0.5 #TODO: Think about... for now it just matches Matlab
+        signal_power_norm = np.linalg.norm(power_(signal))**0.5 #TODO: Think about... for now it just matches Matlab
 
         for noise in self.noise_sources:
             #TODO: figure out why it's divided by 20 and not 10 in that power of 10...
             if noise['noise_type'] == 'relative':
-                state += noise['noise_vector'] * state_scaling / noise['noise_scaling'] * 10**(noise['noise_param'] / 20)
+                signal += noise['noise_vector'] * signal_power_norm / noise['noise_power_norm'] * 10**(-1 * noise['noise_param'] / 20)
             else:
-                state += noise['noise_vector'] * 10**(noise['noise_param'] / 20)
+                signal += noise['noise_vector'] * 10**(noise['noise_param'] / 20)
 
-        return state
+        print(f"Noise power norm unscaled: {noise['noise_power_norm']}")
+        print(f"Signal power norm pure: {signal_power_norm}")
 
-    def display_noise(self):
+
+        return signal
+
+    def display_noisy_signal(self, signal):
         """
-        Displays plots the noise power in the time and frequency domains
+        Displays plots the noisy signal power in the time and frequency domains
+
+        Note that signal WILL set sample_num value
         """
         #TODO: fix up the axes a bit but tbh I just want to see the shape
         _, ax = plt.subplots(2, 1)
 
+        noisy_signal = self.add_noise_to_propagation(signal) 
         x = np.linspace(-1, 1, num=self.sample_num)
-        noise_vector = self.add_noise_to_propagation(np.zeros(self.sample_num, dtype='complex'))
-        ax[0].plot(x, power_(noise_vector), label='time domain')
-        ax[1].plot(x, psd_(noise_vector, 1, 1), label='freq domain')
+
+        ax[0].plot(x, power_(noisy_signal), label='time domain')
+        ax[1].plot(x, 10 * np.log10(psd_(noisy_signal, 1, 1)), label='freq domain')
         ax[0].legend()
         ax[1].legend()
+        plt.title('Noisy signal')
         plt.show()
     
-    def display_noise_sources(self):
+    def display_noise(self, signal):
+        """
+        Displays plots the noise power in the time and frequency domains
+
+        Note that signal WILL set sample_num value
+        """
+        #TODO: fix up the axes a bit but tbh I just want to see the shape
+        _, ax = plt.subplots(2, 1)
+
+        pure_signal = np.copy(signal)
+        noise_vector = self.add_noise_to_propagation(signal) - pure_signal
+        x = np.linspace(-1, 1, num=self.sample_num)
+
+        ax[0].plot(x, power_(noise_vector), label='time domain')
+        ax[1].plot(x, 10 * np.log10(psd_(noise_vector, 1, 1)), label='freq domain (log)')
+        ax[0].legend()
+        ax[1].legend()
+        plt.title('Total noise')
+        plt.show()
+
+    def display_noise_sources_absolute(self):
         """
         Displays plots the power of individual noise contributions in the time and frequency domains
 
         Since no input vector is provided, outputs are displayed as if all noise source options were absolute
+
+        If self._sample_num is None, will display plot for 1000 datapoints
         """
         #TODO: fix up the axes a bit but tbh I just want to see the shape
+
+        display_only_sample = False
+        if (self._sample_num is None):
+            self.sample_num = 1000
+            display_only_sample = True
+    
         _, ax = plt.subplots(2, 1)
 
         x = np.linspace(-1, 1, num=self.sample_num)
@@ -132,22 +183,47 @@ class AdditiveNoise():
             noise_vector = noise['noise_vector'] * noise_param
             ax[0].plot(x, power_(noise_vector), label=f'time domain, param: {noise_param}')
             ax[1].plot(x, psd_(noise_vector, 1, 1), label=f'freq domain, param: {noise_param}')
+        
+        if (display_only_sample): # we only want to remove the sample number if we set it ourselves at the top of the method
+            self._sample_num = None
 
         ax[0].legend()
         ax[1].legend()
+        plt.title('Display each noise source as absolute')
         plt.show()
+    
+    @property
+    def sample_num(self):
+        if (self._sample_num is None):
+            raise ValueError('No sample num set yet')
+        return self._sample_num
+    
+    @sample_num.setter
+    def sample_num(self, n):
+        if (self._seed is not None):
+            np.random.seed(self._seed) # things will be replicable (if single threaded)
+
+        for noise_source in self.noise_sources:
+            noise = np.random.normal(scale=1, size=(n, 2)).view(dtype='complex').flatten()
+            noise_power_norm = np.linalg.norm(power_(noise))**0.5
+            noise_source['noise_vector'] = noise
+            noise_source['noise_power_norm'] = noise_power_norm
+        
+        self._sample_num = n
+
 
 def testing_main():
-    noise = AdditiveNoise(sample_num=10000, noise_type='absolute', noise_param=4)
+    noise = AdditiveNoise(noise_type='absolute', noise_param=4)
     noise.display_noise()
-    noise.add_noise_source(noise_param=2)
+    noise.add_noise_source(noise_param=2) # relative noises
     noise.add_noise_source(noise_param=1)
     noise.add_noise_source(noise_param=0.5)
-    noise.display_noise_sources()
+    noise.display_noise_sources_absolute()
     noise.noise_on = False
     noise.display_noise()
     noise.noise_on = True
+    noise.add_noise_to_propagation(np.array([1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1], dtype='complex'))
     noise.display_noise()
     AdditiveNoise.simulate_with_noise = False
     noise.display_noise()
-    noise.display_noise_sources()
+    noise.display_noise_sources_absolute()
---
+++ testing/test_additiveNoise.py
@@ -0,0 +1,65 @@
+import autograd.numpy as np
+import pytest
+from autograd import grad
+
+from problems.example.assets.additive_noise import AdditiveNoise
+
+@pytest.fixture(autouse=True)
+def setup():
+    AdditiveNoise.simulate_with_noise = True
+
+
+def display_time_freq(noise):
+    x = np.linspace(-2*np.pi, 2*np.pi, 10000)
+    signal = np.sin(x) + 1j*0 # we just really want a visible signal that's not constant power so *shrugs*
+    noise.display_noisy_signal(signal)
+    noise.display_noise(signal)
+    noise.display_noise_sources_absolute()
+
+
+# test not Gaussian and incorrect noise type
+def test_incorrect_distribution():
+    with pytest.raises(ValueError):
+        AdditiveNoise(distribution='lorentzian')
+
+
+def test_incorrect_noise_type():
+    with pytest.raises(ValueError):
+        AdditiveNoise(noise_type='transcendent')
+
+
+# simple test, visually assess whether the time-freq domain look correct
+def test_display_timeFreq():
+    # noise param = 1
+    noise1 = AdditiveNoise(noise_param=1, seed=0)
+    display_time_freq(noise1)
+
+    # noise param = 5
+    noise5 = AdditiveNoise(noise_param=5, seed=0)
+    display_time_freq(noise5)
+
+    # noise param = 10
+    noise10 = AdditiveNoise(noise_param=10, seed=0)
+    display_time_freq(noise10)
+
+    # noise param = 32
+    noise32 = AdditiveNoise(noise_param=32, seed=0)
+    display_time_freq(noise32)
+
+# test multiple noise sources in one
+def test_multiple_noise_sources():
+    pass
+
+# test absolute vs relative (with new object)
+
+# test incorrect noise type
+
+# test add_noise_to_propagation
+    
+# test that noise arrays are not recomputed everytime add_noise_to_propagation is added
+
+# test noise_on and simulate with noise
+
+# test incompatible shapes
+
+# test autograd capabilities
