347789fdc287c8183ae72bdf719cfc431aac4b95
Benjamin MacLellan <ben.mac@live.ca>
2020-09-14 14:36:48 -0400

    Added saving/loading functionality for graphs/metadata

--- config/config.py
+++ config/config.py
@@ -2,22 +2,11 @@
 
 """
 
-# this sets which numpy library to use throughout - either numpy or autograd.numpy
-import autograd.numpy as np   # TODO: maybe just always use autograds wrapping of numpy
-import scipy as sp
-
 import matplotlib.pyplot as plt
 import os
-# set plotting style
-
 plt.style.use(os.path.join(os.path.dirname(__file__), r"plot-style.mplstyle"))
-
-# TODO: dynamically choose which simulation library to use
 
 # plug-in dictionaries to collect available functions/classes
 EVOLUTION_OPERATORS = dict()
 NODE_TYPES = dict()
 NODE_TYPES_ALL = dict()
-
-# LOG_DIRECTORY = os.get
-# LOG_DIRECTORY = r"C:\Users\benjamin\Documents\INRS - Projects\asope\logs"
--- problems/example/assets/functions.py
+++ problems/example/assets/functions.py
@@ -59,21 +59,12 @@ def get_pulse_train(t, pulse_rep_t, pulse):
     :param pulse:
     :return:
     """
-    # def sech(t, width):
-    #     return 1 / np.cosh(t / width, dtype='complex')
-    #
-    # def gaussian(t, width):
-    #     return np.exp(-0.5 * (np.power(t / width, 2)), dtype='complex')
 
     dt = t[1] - t[0]
     impulse_inds = np.arange(pulse_rep_t / dt * 0.5, pulse_rep_t / dt * (np.round((t[-1] - t[0]) / pulse_rep_t) + 0.5), pulse_rep_t / dt).astype('int')
     impulses = np.zeros_like(t).astype('complex')
     impulses[impulse_inds] = 1
-    # if pulse_shape == 'gaussian':
-    #     pulse = gaussian(t, pulse_width)
-    # elif pulse_shape == 'sech':
-    #     pulse = sech(t, pulse_width)
-    pulse_train = numpy.fft.ifft(numpy.fft.fft(pulse, axis=0) * numpy.fft.fft(impulses, axis=0), axis=0)
 
+    pulse_train = numpy.fft.ifft(numpy.fft.fft(pulse, axis=0) * numpy.fft.fft(impulses, axis=0), axis=0)
     return pulse_train
 
