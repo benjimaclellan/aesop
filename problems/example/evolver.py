200474f6137d1fc290d96e0866699f0ba54aa528
Julie Belleville <julie@DESKTOP-MRCH6LM.localdomain>
2020-10-20 17:26:11 -0700

    added framework for different evolution operator selection '

--- problems/example/evolver.py
+++ problems/example/evolver.py
@@ -1,4 +1,5 @@
-import autograd.numpy as np 
+import autograd.numpy as np
+import random
 
 from .evolution_operators.evolution_operators import *
 import config.config as configuration
@@ -7,23 +8,48 @@ import config.config as configuration
 class Evolver(object):
     """
     """
-    def __init__(self, verbose=False, **attr):
+    def __init__(self, verbose=False, operator_likelihood=None, **attr):
+        """
+        :param operator_likelihood: likelihood weighting the probability of each evolution operator being applied. If None, probability of each is equal
+                                    These likelihoods are not required to be probabilities: they are rescaled, but their relative values matter
+                                    operator_likelihood must be provided as a dictionary with (key, val) = (operator_class_name, likelihood)
+                                    likelihood can be a float, or a scalar function of the current generation, but the type must be consistent for each operator name (i.e. all floats, or all funcs)
+        """
         self.verbose = verbose
+        self.time_dependent_likelihood = False
+        if operator_likelihood is None:
+            self.operator_likelihood = [1] *  len(configuration.EVOLUTION_OPERATORS)
+        elif callable(operator_likelihood.values()[0]):
+            # TODO: add warning if we're trying to give a likelihood to an operator that does not exist
+            self.time_dependent_likelihood = True
+            # maps generation to an array of the likelihood at time gen
+            self.operator_likelihood = lambda gen :[operator_likelihood[operator.__class__.__name__](gen) for operator in configuration.EVOLUTION_OPERATORS.values()]
+        else:
+            self.operator_likelihood = [operator_likelihood[operator.__class__.__name__] for operator in configuration.EVOLUTION_OPERATORS.values()]
+
         super().__init__(**attr)
         return
 
 
-    def evolve_graph(self, graph, evaluator, verbose=False):
+    def evolve_graph(self, graph, evaluator, generation=None, verbose=False):
         """
         Function
         """
-
         # check if each evolution operator is possible
         verification = [evo_op().verify_evolution(graph) for (_, evo_op) in configuration.EVOLUTION_OPERATORS.items()]
 
         # choose one evolution from all possible
         possible_evo_ops = [evo_op for (verify, evo_op) in zip(verification, configuration.EVOLUTION_OPERATORS.values()) if verify]
-        evo_op_choice = np.random.choice(possible_evo_ops)
+
+        if self.time_dependent_likelihood:
+            if generation is None:
+                raise ValueError('Generation must be provided for time dependent likelihoods')
+            probability = np.array([prob for (prob, verify) in zip(self.operator_likelihood(generation), verification) if verify])
+        else:
+            probability = np.array([prob for (prob, verify) in zip(self.operator_likelihood, verification) if verify])
+
+        probability = probability / np.sum(probability)
+        evo_op_choice = np.random.choice(possible_evo_ops, p=probability)
 
         # apply the chosen evolution
         graph = evo_op_choice().apply_evolution(graph, verbose=self.verbose)
@@ -44,7 +70,49 @@ class Evolver(object):
         return graph
 
 
-class CrossoverMaker():
+class RuleBasedEvolver(Evolver):
+    """
+    Evolves according to a set of rules with a certain probability, and randomly with another probability
+
+    The trend is that the exploitativeness of the algorithm will increase over time, as exploration decreases:
+    https://towardsdatascience.com/striking-a-balance-between-exploring-and-exploiting-5475d9c1e66e
+    https://www.manifold.ai/exploration-vs-exploitation-in-reinforcement-learning
+
+    We assume that the decay in exploitation is linear, for simplicity
+    """
+    def __init__(self, epsilon_start=0.6, epsilon_end=0.1, decay_length_gen=10, verbose=False, operator_likelihood=None, **attr):
+        """
+        Rule based evolver evolves randomly with probability epsilon, and based on the rules with probability 1 - epsilon
+        """
+        def epsilon_from_gen(gen):
+            if gen > decay_length_gen:
+                return epsilon_end
+            else:
+                return (epsilon_end - epsilon_start) / decay_length_gen * gen + epsilon_start
+
+        self.epsilon_from_gen = epsilon_from_gen
+
+        super().__init__(verbose=verbose, operator_likelihood=operator_likelihood, **attr)
+    
+    def evolve_graph(self, graph, evaluator, generation=None, verbose=False):
+        if random.random() < self.epsilon_from_gen(generation):
+            return super().evolve_graph(graph, evaluator, generation=generation, verbose=verbose)
+        
+        evo_op_choice = self.select_correct_evolution(graph)
+        # apply the chosen evolution
+        graph = evo_op_choice().apply_evolution(graph, verbose=self.verbose)
+
+        # maybe run hessian analysis here, maybe we can do something with it, maybe not (could have two classes)
+        return graph, evo_op_choice
+    
+    def select_correct_evolution(self, graph):
+        # TODO: have some selection rule! this is currently here as a stopgap
+        verification = [evo_op().verify_evolution(graph) for (_, evo_op) in configuration.EVOLUTION_OPERATORS.items()]
+        possible_evo_ops = [evo_op for (verify, evo_op) in zip(verification, configuration.EVOLUTION_OPERATORS.values()) if verify]
+        return np.random.choice(possible_evo_ops)
+
+
+class CrossoverMaker(object):
     """
     An explicit class is not really necessary right now, but might become useful if we define more crossover operators
     """
---
+++ scripts/run_evolver_tests.py
@@ -0,0 +1,23 @@
+"""
+These tests are only really around for debugging: the implementation isn't terribly complicated
+so we don't expect to have to repeatedly test it
+"""
+
+def test_equal_prob():
+    # TODO: sample repeatedly, make a histogram (visual)
+    pass
+
+
+def test_offset_prob():
+    # TODO: sample repeatedly, make a histogram
+    pass
+
+
+def test_time_dependent_prob():
+    # TODO: histogram, where time dependence is a step function (easy to see)
+    pass 
+
+if __name__=="__main__":
+    test_equal_prob()
+    test_offset_prob()
+    test_time_dependent_prob()
