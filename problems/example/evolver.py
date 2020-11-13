fixed hessian related bugs

problems/example/evolver.py
@@ -10,6 +10,7 @@ import problems.example.assets.hessian_graph_analysis as hessian_analysis
 from .evolution_operators.evolution_operators import *
 import config.config as configuration
 
+#TODO: fix the reinforcement learning one...
 
 class Evolver(object):
     """
@@ -88,45 +89,49 @@ class ProbabilityLookupEvolver(object):
     This class has the most basic probability selection: if a given operator can possibly be run on a node/edge, it will be assigned a value of one.
     If it is not possible, it is assigned a value of zero. Probability are normalized prior to selecting the operator
     """
-    def __init__(self, verbose=False, **attr):
+    def __init__(self, verbose=False, debug=False, **attr):
         self.verbose = verbose
         self.evo_op_list = list(configuration.EVOLUTION_OPERATORS.values()) # we pick these out because technically dictionary values are not ordered
                                                                             # so because we need our matrix order to be consistent, 
+        self.debug = debug
         super().__init__(**attr)
     
-    def evolve_graph(self, graph, evaluator, generation=None, verbose=False, debug=False):
+    def evolve_graph(self, graph, evaluator, generation=None):
         """
         Evolves graph according to the stochastic matrix probabilites
 
         :param graph: the graph to evolve
         :param evaluator: not used in the base implementation, but may be useful in the future
         """
-        if graph.evo_probabilities_matrix is None:
-            self.create_graph_matrix(graph, evaluator)
-        
-        if debug:
-            print(f'evolution probability matrix for graph {graph}')
+
+        # note: graph update has been moved to the start of the evolution function rather than the end BECAUSE the Hessian
+        # evolvers need to function on the already optimized parameter space
+
+        self.update_graph_matrix(graph, evaluator)
+
+        if self.verbose:
+            print(f'evolving graph:')
+            print(graph)
+
+        if self.debug:
+            print(f'evolution probability matrix for graph')
             print(graph.evo_probabilities_matrix)
             print()
 
-        print(f'evolving graph: {graph.nodes}')
-        for node in graph.nodes:
-            print(graph.nodes[node]['model'])
         node_or_edge, evo_op = graph.evo_probabilities_matrix.sample_matrix()
-        graph = evo_op().apply_evolution_at(graph, node_or_edge, verbose=verbose)
-        print(f'\nevolving on: {node_or_edge}, with operator: {evo_op}\n')
-        self.update_graph_matrix(graph, evaluator, evo_op, node_or_edge)
-        print(f'evolved graph: {graph.nodes}')
-        for node in graph.nodes:
-            print(graph.nodes[node]['model'])
-        print()
+        graph = evo_op().apply_evolution_at(graph, node_or_edge, verbose=self.verbose)
+        
+        if self.verbose:
+            print(f'\nevolving on: {node_or_edge}, with operator: {evo_op}\n')
+        
+        if self.verbose:
+            print(f'evolved graph:')
+            print(graph)
+            print()
        
         x, *_, lower_bounds, upper_bounds = graph.extract_parameters_to_list()
-        for i in range(len(x)):
-            while type(x[i]) == ArrayBox:
-                x[i] = x[i]._value
 
-        assert np.logical_and(lower_bounds < x, x < upper_bounds).all(), f'lower bound: {lower_bounds}\n params: {x}\n upperbounds: {upper_bounds}' #' \n pre-swap param: {pre_swap_params}\n new_node params: {list(zip(new_model.parameter_names, new_model.parameters))}'
+        assert np.logical_and(lower_bounds <= x, x <= upper_bounds).all(), f'lower bound: {lower_bounds}\n params: {x}\n upperbounds: {upper_bounds}' #' \n pre-swap param: {pre_swap_params}\n new_node params: {list(zip(new_model.parameter_names, new_model.parameters))}'
 
         return graph, evo_op
 
@@ -144,18 +149,18 @@ class ProbabilityLookupEvolver(object):
         graph.evo_probabilities_matrix.normalize_matrix()
         graph.evo_probabilities_matrix.verify_matrix()
 
-    def update_graph_matrix(self, graph, evaluator, last_evo_op, last_node_or_edge):
+    def update_graph_matrix(self, graph, evaluator):
         """
         Function does not use evaluator, last_evo_op, or last_node_or_edge in this implementation. However, they may be valid parameters to modify later
         """
         self.create_graph_matrix(graph, evaluator) # just fully remakes the graph matrix for now
 
-    def random_graph(self, graph, evaluator, n_evolutions=10, view_evo=False, verbose=False, debug=False):
+    def random_graph(self, graph, evaluator, n_evolutions=10, view_evo=False):
         for n in range(n_evolutions):
-            if verbose:
+            if self.verbose:
                 print(f'Starting evolution number {n}')
             try:
-                graph_tmp, evo_op = self.evolve_graph(graph, evaluator, generation=n, verbose=verbose, debug=debug)
+                graph_tmp, evo_op = self.evolve_graph(graph, evaluator, generation=n)
                 graph_tmp.assert_number_of_edges()
                 graph = graph_tmp
                 if view_evo:
@@ -253,7 +258,7 @@ class SizeAwareLookupEvolver(ProbabilityLookupEvolver):
        and 1 + alpha to reduction operators. Alpha is clipped at +=0.9 by default, but can be clipped at any amplitude between 0 and 1 if desired
     4. alpha = alpha(delta) is an odd function (where alpha < 0 where delta < 0), where delta = <# of nodes in the graph> - <ideal # of nodes>. alpha(0) = 0
     """
-    def __init__(self, verbose=False, ideal_node_num=8, alpha_func=lambda delta: (delta / 10)**3, alpha_bound=0.9, **attr):
+    def __init__(self,ideal_node_num=8, alpha_func=lambda delta: (delta / 10)**3, alpha_bound=0.9, **attr):
         """
         Initializes SizeAwareMatrixEvolver (see class description above)
 
@@ -266,7 +271,7 @@ class SizeAwareLookupEvolver(ProbabilityLookupEvolver):
         Only the ideal node number is intended to be updated over the course of the object's existence (there's a useful case for it I can see, unlike the other params)
         That said, updates to any of these parameters is possible (i.e. the code will use new alpha_func and alpha_max if their values are changed)
         """
-        super().__init__(verbose=verbose, **attr)
+        super().__init__(**attr)
         self.ideal_node_num = ideal_node_num
         self.alpha_func = alpha_func
 
@@ -331,7 +336,7 @@ class ReinforcementLookupEvolver(ProbabilityLookupEvolver):
     # TODO: consider making value less based on immediate reward
     # TODO: consider decaying epsilon scheme instead of constant epsilon value
     """
-    def __init__(self, verbose=False, starting_value_matrix=None, epsilon=0.4, **attr):
+    def __init__(self, starting_value_matrix=None, epsilon=0.4, **attr):
         """
         Creates a ReinforcementMatrixEvolver (reinforcement is used a bit loosely here)
 
@@ -345,7 +350,7 @@ class ReinforcementLookupEvolver(ProbabilityLookupEvolver):
 
                                       If a string, this is the string to a pickle file of a previous run
         """
-        super().__init__(verbose=verbose, **attr)
+        super().__init__(**attr)
         self.epsilon = epsilon
         
         if starting_value_matrix is None:
@@ -430,7 +435,7 @@ class ReinforcementLookupEvolver(ProbabilityLookupEvolver):
         # 2. no max valued option is available
         raise ValueError('No possible operator found')
 
-    def evolve_graph(self, graph, evaluator, generation=None, verbose=False, debug=False):
+    def evolve_graph(self, graph, evaluator, generation=None):
         """
         Evolves graph according to the stochastic matrix probabilites
         The probabilities for operators are based on a epsilon-greedy learning method
@@ -440,7 +445,7 @@ class ReinforcementLookupEvolver(ProbabilityLookupEvolver):
         :param evaluator: not used in the base implementation, but may be useful in the future
         """
         pre_evo_state = len(graph.nodes)
-        graph, evo_op = super().evolve_graph(graph, evaluator, generation=generation, verbose=verbose, debug=debug)
+        graph, evo_op = super().evolve_graph(graph, evaluator, generation=generation)
         if graph.last_evo_record is None:
             graph.last_evo_record = {}
         graph.last_evo_record['op'] = evo_op
@@ -485,8 +490,8 @@ class EGreedyHessianEvolver(ProbabilityLookupEvolver):
 
     Other operators: V = e = <default_val>
     """
-    def __init__(self, verbose=False, epsilon=1, freewheel_removal_coeff=1, terminal_removal_coeff=0.5, terminal_duplicate_coeff=1,
-                freewheel_path_removal_coeff=0.3, terminal_path_removal_coeff=2, default_val=1, **attr):
+    def __init__(self, epsilon=1, freewheel_removal_coeff=1, terminal_removal_coeff=0.5, terminal_duplicate_coeff=1,
+                freewheel_path_removal_coeff=0.3, terminal_path_removal_coeff=5, default_val=1, **attr):
         """
         Creates a Hessian based evolver (which simplifies graphs based on the Hessian)
 
@@ -496,7 +501,6 @@ class EGreedyHessianEvolver(ProbabilityLookupEvolver):
 
         """
         if type(epsilon) == float or type(epsilon) == int:
-            # self.epsilon = lambda gen: epsilon
             self.epsilon = epsilon
         else:
             AssertionError('function based epsilon not implemented yet')
@@ -510,7 +514,7 @@ class EGreedyHessianEvolver(ProbabilityLookupEvolver):
         self.terminal_path_removal_coeff = terminal_path_removal_coeff
         self.default_val = default_val
 
-        super().__init__(verbose=verbose, **attr)
+        super().__init__(**attr)
     
 
     def create_graph_matrix(self, graph, evaluator):
@@ -525,34 +529,7 @@ class EGreedyHessianEvolver(ProbabilityLookupEvolver):
         value_matrix = self._get_action_value_matrix(graph, evaluator) # we don't actually need the matrix, just the top val.
                                                                        # BUT it's useful to see the full matrix for tuning/debugging purposes 
         greedy_node_or_edge, greedy_op = value_matrix.get_largest_prob_node_or_edge_and_op()
-        # try:
-        #     if len(x) == 0:
-        #         print('BIG NEWS, LENGTH X IS ZERO')
-        #     else:
-        #         print(f'len(x): {len(x)}')
 
-        #     print(f'wrapped_param: {x}')
-        #     for i in range(len(x)):
-        #         while type(x[i]) == ArrayBox:
-        #             print(f'unwrap value {i}')
-        #             x[i] = x[i]._value
-        #     print(f'unwrapped params: {x}')
-        #     graph.grad(x)
-
-        #     # jacobian_funct = jacobian(graph.func)
-        #     # print(f'jacobian: {jacobian_funct(np.array(x))}')
-        # except NotImplementedError as k:
-        #     print(k)
-        #     print('Graph config')
-        #     for node in graph.nodes:
-        #         print(graph.nodes[node]['model'])
-
-        #     # print(f'wrapped_param: {x}')
-        #     # for i in range(len(x)):
-        #     #     while type(x[i]) == ArrayBox:
-        #     #         x[i] = x[i]._value
-        #     # print(f'unwrapped params: {x}')
-        #     raise(k)
         non_greedy_prob = self.epsilon / np.sum(graph.evo_probabilities_matrix.matrix)
 
         for node_or_edge in list(graph.nodes) + list(graph.edges):
@@ -569,14 +546,10 @@ class EGreedyHessianEvolver(ProbabilityLookupEvolver):
         
         graph.evo_probabilities_matrix.normalize_matrix()
         graph.evo_probabilities_matrix.verify_matrix()
-        # print(f'Graph node models, and parameters')
-        # for node in graph.nodes:
-        #     print(graph.nodes[node]['model'])
-        # print(f'start value x: {x}')
-        # end_param, *_ = graph.extract_parameters_to_list()
-        # print(f'end_params: {x}')
+        if self.debug:
+            print(f'value matrix:')
+            print(value_matrix)
 
-    
     def _get_action_value_matrix(self, graph, evaluator):
         """
         The matrix is actually not needed for the implementation, but it's def needed for tuning hyperparameters and debugging
@@ -593,20 +566,13 @@ class EGreedyHessianEvolver(ProbabilityLookupEvolver):
             - A free-wheeling node is likely doing nothing, so might as well boot it
             - Also, a terminal node Might be in passive state, so we can try booting it as well
         """
-        # TODO: handle cases where there were no unlocked param, and so terminal_node_scores[node_or_edge] is not defined
-        # print(f'before hessian analysis...')
-        # for node in graph.nodes:
-        #     print(graph.nodes[node]['model'])
-        # x, *_ = graph.extract_parameters_to_list()
-        # print(f'before hessian functs params: {x}\n')
-        # for i in range(len(x)):
-        #     while type(x[i]) == ArrayBox:
-        #         x[i] = x[i]._value
-        # print(f'before hessian unwrapped params: {x}\n\n')
+        # ------------ test to see if it'll crash on the first grad and hess
+        x, *_ = graph.extract_parameters_to_list()
+        graph.grad(x)
+        graph.hess(x)
+        # --------------- end test
+
         terminal_node_scores, free_wheeling_node_scores = hessian_analysis.get_all_node_scores(graph)
-        # print(f'after hessian analysis...')
-        # x, *_ = graph.extract_parameters_to_list()
-        # print(f'after hessian functs params: {x}\n')
 
         value_matrix = self.ProbabilityMatrix(self.evo_op_list, list(graph.nodes), list(graph.edges)) # using this to build our value matrix
 

problems/example/graph.py
@@ -68,11 +68,11 @@ class Graph(GraphParent):
 
         self.score = None
     
-    # def __str__(self):
-    #     string_rep = ""
-    #     for i in self.nodes:
-    #         string_rep += f"{self.nodes[i]['model'].node_acronym} "
-    #     return string_rep
+    def __str__(self):
+        str_rep = ''
+        for node in self.nodes:
+            str_rep += f"{node}: {self.nodes[node]['model']}\n"
+        return str_rep
 
     def function_wrapper(self, propagator, evaluator, exclude_locked=True):
         """ returns a function handle that accepts only parameters and returns the score. used to initialize the hessian analysis """
@@ -105,14 +105,7 @@ class Graph(GraphParent):
 
         scale_matrix = np.matmul(parameter_imprecisions, parameter_imprecisions.T)
 
-        def _scaled_hess(parameters):
-            # print(f'in scaled_hess, scale matrix: {scale_matrix}')
-            print(f'comparing shapes, param: {len(parameters)}, scale_matrix: {scale_matrix.shape}')
-            return self.hess(np.array(parameters)) * scale_matrix
-
-        return _scaled_hess
-        # return lambda parameters: self.hess(np.array(parameters)) * scale_matrix
- 
+        return lambda parameters: self.hess(np.array(parameters)) * scale_matrix 
 
     def get_in_degree(self, node):
         return len(self.get_in_edges(node))