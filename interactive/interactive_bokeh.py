432fdeb9a7155f6a4a036a90a22afc3e5e2620ae
Benjamin MacLellan <ben.mac@live.ca>
2020-11-22 18:18:50 -0500

    Basic interaction with graph added to interactive_bokeh.py

--- interactive/interactive_bokeh.py
+++ interactive/interactive_bokeh.py
@@ -11,25 +11,18 @@ import pathlib
 import os
 import platform
 import copy
-
 import dill
-
-parent_dir = str(pathlib.Path(__file__).absolute().parent.parent)
-sep = ';' if platform.system() == 'Windows' else ':'
-os.environ["PYTHONPATH"] = parent_dir + sep + os.environ.get("PYTHONPATH", "")
-sys.path.append(parent_dir)
-
 import numpy as np
 import copy
 import random
 import string
 import math
+import networkx as nx
+import itertools
 
 from bokeh.layouts import gridplot, column, row
 from bokeh.models import BoxSelectTool, LassoSelectTool
 from bokeh.plotting import curdoc, figure
-
-import networkx as nx
 
 from bokeh.io import output_file, show
 from bokeh.models import (BoxSelectTool, Circle, EdgesAndLinkedNodes, HoverTool,
@@ -41,24 +34,54 @@ from bokeh.palettes import Spectral4
 from bokeh.plotting import from_networkx
 from bokeh.models import ColumnDataSource
 from bokeh.models import Button, CustomJS, FileInput, TextInput, Select, Slider, Div
+from bokeh.palettes import Spectral6
+from bokeh.models import Paragraph
+from bokeh.models import ColumnDataSource, DataTable, DateFormatter, TableColumn, HTMLTemplateFormatter, NumberFormatter
 
-from model import VisualGraph
+parent_dir = str(pathlib.Path(__file__).absolute().parent.parent)
+sep = ';' if platform.system() == 'Windows' else ':'
+os.environ["PYTHONPATH"] = parent_dir + sep + os.environ.get("PYTHONPATH", "")
+sys.path.append(parent_dir)
+
+from model import AccessGraph
+from problems.example.assets.functions import fft_, ifft_, psd_, power_
+from problems.example.graph import Graph
+from problems.example.assets.propagator import Propagator
 
 #%% callbacks
-DEBUG = True
-
-
-def update():
-    plots['prop_time']['source'].data = dict(new_points().data)
-    plots['prop_freq']['source'].data = dict(new_points().data)
-    plots['tran_time']['source'].data = dict(new_points().data)
-    plots['tran_freq']['source'].data = dict(new_points().data)
-
 
 def edge_callback(attr,old,new):
+    global main_graph
+    main_graph.propagate(propagator, save_transforms=True)
+
     if DEBUG: print(f"edge select {attr, old, new}")
-    update()
-    update_textbox(attr, old, new)
+    # print(graph_renderer.edge_renderer.data_source.data)
+    text = ''
+
+    for index in new:
+        edge = graph_renderer.edge_renderer.data_source.data['edge'][index]
+        text +=  f"Selected edge model: {main_graph.edges[edge]['model'].node_acronym}\n\n"
+
+        table_source.data = dict(parameters=main_graph.edges[edge]['model'].parameters,
+                                 parameter_names=main_graph.edges[edge]['model'].parameter_names)
+
+        dropdown.options = main_graph.edges[edge]['model'].parameter_names
+
+    # here we change which lines are visible based on the selection policy of the graph
+    for plot_id, plot in plots.items():
+        for index in new:
+            edge = graph_renderer.edge_renderer.data_source.data['edge'][index]
+            if edge in plot['lines'].keys():
+                for line in plot['lines'][edge]:
+                    line.visible = True
+
+        for index in old:
+            edge = graph_renderer.edge_renderer.data_source.data['edge'][index]
+            if edge in plot['lines'].keys():
+                for line in plot['lines'][edge]:
+                    line.visible = False
+
+    div.text = text
 
 
 def text_summary_node(model):
@@ -66,34 +89,77 @@ def text_summary_node(model):
     return summary
 
 
-def node_callback(attr,old,new):
+def node_callback(attr, old, new):
     if DEBUG: print(f"node select {attr, old, new}")
     text = ''
     for node in new:
         text += text_summary_node(main_graph.nodes[node]['model'])
-    div.text = text
-    # print(graph_renderer.node_renderer.data_source.data)
-    # div.text = f'node something: {main_graph.nodes[new]}'
 
 
 def load_graph():
+    global propagator
+    global main_graph
+    global plots
+    global palette
+
+    for plot_id, plot in plots.items():
+        for id, lines in plot['lines'].items():
+            for line in lines:
+                plot['plot'].renderers.remove(line)
+        plot['sources'] = {}
+        plot['lines'] = {}
+
+
     filepath = filepath_input.value
     verbose = True
     if verbose: print(f'Loading graph from {filepath}')
     with open(filepath, 'rb') as file:
         full_graph = dill.load(file)
-    full_graph = nx.convert_node_labels_to_integers(full_graph, first_label=0, ordering='default', label_attribute=None)
-    return full_graph
+    main_graph = nx.convert_node_labels_to_integers(full_graph, first_label=0, ordering='default', label_attribute=None)
+
+    prop_filepath = pathlib.Path(filepath).parent.joinpath('propagator.pkl')
+    if verbose: print(f"Loading propagator from {prop_filepath}")
+    with open(prop_filepath, 'rb') as file:
+        propagator = dill.load(file)
+
+    main_graph.propagate(propagator, save_transforms=True)
+
+    for edge in main_graph.edges:
+        state = main_graph.measure_propagator(edge)
+
+        plots['prop_time']['sources'][edge] = [ColumnDataSource(data=dict(x=propagator.t, y=power_(state).astype('float')))]
+        plots['prop_freq']['sources'][edge] = [ColumnDataSource(data=dict(x=propagator.f, y=np.log10(psd_(state, dt=propagator.dt, df=propagator.df).astype('float'))))]
+
+        plots['tran_time']['sources'][edge] = []
+        plots['tran_freq']['sources'][edge] = []
+
+        if main_graph.edges[edge]['model'].transform is not None:
+            for (dof, transform, label) in main_graph.edges[edge]['model'].transform:
+                if dof == 't':
+                    plots['tran_time']['sources'][edge].append(ColumnDataSource(data=dict(x=propagator.t, y=transform.astype('float'))))
+                elif dof == 'f':
+                    plots['tran_freq']['sources'][edge].append(ColumnDataSource(data=dict(x=propagator.f, y=transform.astype('float'))))
+
+
+    for plot_id, plot in plots.items():
+        for id, sources in plot['sources'].items():
+            plot['lines'][id] = []
+            for new_source in sources:
+                new_line = plot['plot'].line(x='x', y='y', source=new_source, alpha=0.8, line_color=next(palette), line_width=3.0, visible=False)
+                plot['lines'][id] += [new_line]
+
+    return
 
 
 def button_callback():
-    main_graph = load_graph()
-    disp_graph = VisualGraph.from_asope_graph(main_graph)
-    node_dict, edge_dict = disp_graph.export_as_dict()
+    global main_graph
+    global disp_graph
 
-    update_node_positions(disp_graph, node_dict, edge_dict)
-    div.text = f"File: {filepath_input.value}, Graph cardinality: {disp_graph.number_of_nodes()}"
-    # div.text = f"something new {random.choice(string.ascii_lowercase)}"
+    load_graph()
+
+    node_dict, edge_dict = disp_graph.export_as_dict(main_graph)
+    update_node_positions(disp_graph.get_layout(main_graph), node_dict, edge_dict)
+    div.text = f"File: {filepath_input.value}, Graph cardinality: {main_graph.number_of_nodes()}"
 
 
 def file_input_callback(attr,old,new):
@@ -111,30 +177,13 @@ def slider_callback(attr, old, new):
 def dropdown_callback(attr, old, new):
     if DEBUG: print(f'select {attr, old, new}')
 
-
-def new_points():
-    x = np.linspace(-5, 5, 500)
-    y = 5 * np.exp(-x**2) + np.random.uniform(0.0, 1.5, x.shape[0])
-    return ColumnDataSource(data=dict(x=x, y=y))
-
-
-def convert_from_networkx(graph_nx):
-    pass
-
-
-def random_graph():
-    G = nx.random_regular_graph(np.random.randint(1, 4), np.random.choice([6,8,10]))
-    return G
-
-
-def update_node_positions(_graph, node_dict, edge_dict):
+def update_node_positions(layout, node_dict, edge_dict):
 
     graph_renderer.node_renderer.data_source.data = node_dict
     graph_renderer.edge_renderer.data_source.data = edge_dict
 
-    graph_layout = nx.kamada_kawai_layout(_graph)
     # graph_layout = main_graph.optical_system_layout()
-    graph_renderer.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
+    graph_renderer.layout_provider = StaticLayoutProvider(graph_layout=layout)
 
 
 #%%
@@ -142,8 +191,7 @@ def update_node_positions(_graph, node_dict, edge_dict):
 button = Button(label="Load graph", button_type="success")
 button.on_click(button_callback)
 
-filepath_input = TextInput(value=r'C:\Users\benjamin\Documents\INRS - Projects\asope_data\testing\20201120_FEe2\graph_hof0.pkl', title='')
-# filepath_input = TextInput(value='path/to/graph/file/graph.pkl', title='')
+filepath_input = TextInput(value=r'C:\Users\benjamin\Documents\INRS - Projects\asope_data\interactive\example\test_graph.pkl', title='')
 filepath_input.on_change('value', file_input_callback)
 
 dropdown = Select(title="Parameter to control:", value="option1", options=["option1", "option2", "option3"])
@@ -152,14 +200,30 @@ dropdown.on_change('value', dropdown_callback)
 slider = Slider(start=0.1, end=1.0, value=1, step=.1, title="Parameter value")
 slider.on_change('value', slider_callback)
 
-div = Div(text="""this is a HTML Div where the node info will be printed when an edge or node is selected in the graph""", sizing_mode='stretch_both')
+table_source = ColumnDataSource(data=dict(parameter_names=[],
+                                          parameters=[],))
 
-widgets = column(filepath_input, button, dropdown, slider, width=200, height=100)
+template="""
+<b><div style="background:<%= "NavajoWhite" %>;">
+<%= (value).toExponential(4) %></div></b>
+"""
+formatter = HTMLTemplateFormatter(template=template)
+columns = [TableColumn(field="parameters", title="Parameter", formatter=formatter),
+           TableColumn(field="parameter_names", title="Parameter Name"),]
+data_table = DataTable(source=table_source, columns=columns, width=400, height=300, sizing_mode='stretch_both')
+
+div = Paragraph(text="""this is a HTML Div where the node info will be printed when an edge or node is selected in the graph""", sizing_mode='stretch_both')
+
+widgets = column(filepath_input, button, dropdown, slider, width=200, height=300)
 
 #%% main graph
 
-main_graph = load_graph()
-disp_graph = VisualGraph.from_asope_graph(main_graph)
+DEBUG = True
+palette = itertools.cycle(Spectral6)
+
+propagator = None
+main_graph = Graph()
+disp_graph = AccessGraph()
 
 
 plot_graph = Plot(sizing_mode="stretch_both", width=600,
@@ -170,9 +234,8 @@ plot_graph.add_tools(hover, TapTool(), BoxSelectTool())
 
 graph_renderer = GraphRenderer()
 
-node_dict, edge_dict = disp_graph.export_as_dict()
-update_node_positions(disp_graph, node_dict, edge_dict)
-
+node_dict, edge_dict = disp_graph.init_dict()
+update_node_positions(disp_graph.get_graph_positions(main_graph), node_dict, edge_dict)
 
 ## set interaction visuals
 graph_renderer.node_renderer.glyph = Circle(size=15, fill_color=Spectral4[0])
@@ -185,8 +248,8 @@ graph_renderer.edge_renderer.hover_glyph = MultiLine(line_color=Spectral4[1], li
 
 # graph_renderer.inspection_policy = EdgesAndLinkedNodes()
 graph_renderer.inspection_policy = NodesOnly()
-# graph_renderer.selection_policy = EdgesAndLinkedNodes()
-graph_renderer.selection_policy = NodesOnly()
+graph_renderer.selection_policy = EdgesAndLinkedNodes()
+# graph_renderer.selection_policy = NodesOnly()
 graph_renderer.node_renderer.data_source.selected.on_change("indices", node_callback)
 graph_renderer.edge_renderer.data_source.selected.on_change("indices", edge_callback)
 
@@ -196,10 +259,10 @@ plot_graph.renderers.append(graph_renderer)
 #%% temporal electric field
 
 plots = {
-    'prop_time':{'title':'Optical Intensity', 'xlabel':'Time', 'ylabel':'a.u.', 'source':new_points()},
-    'prop_freq':{'title':'', 'xlabel':'Freq', 'ylabel':'a.u.', 'source':new_points()},
-    'tran_time':{'title':'Transfer Functions', 'xlabel':'Time', 'ylabel':'a.u.', 'source':new_points()},
-    'tran_freq':{'title':'', 'xlabel':'Freq', 'ylabel':'a.u.', 'source':new_points()},
+    'prop_time':{'title':'Optical Intensity', 'xlabel':'Time', 'ylabel':'a.u.', 'sources':{}, 'lines':{}},
+    'prop_freq':{'title':'', 'xlabel':'Freq', 'ylabel':'a.u.', 'sources':{}, 'lines':{}},
+    'tran_time':{'title':'Transfer Functions', 'xlabel':'Time', 'ylabel':'a.u.', 'sources':{}, 'lines':{}},
+    'tran_freq':{'title':'', 'xlabel':'Freq', 'ylabel':'a.u.', 'sources':{}, 'lines':{}},
 }
 for plot, info in plots.items():
     pfig = figure(tools=[tools.PanTool(), tools.BoxZoomTool(), tools.ResetTool(), tools.SaveTool()],
@@ -207,15 +270,17 @@ for plot, info in plots.items():
                   x_axis_label=info['xlabel'], y_axis_label=info['ylabel'],
                   toolbar_location="left", title=info['title'], title_location='above')
     pfig.background_fill_color = "#fafafa"
-    r = pfig.line(x='x', y='y', source=info['source'], alpha=0.6)
     info['plot'] = pfig
 
+# this will lock the ranges together for comparing plots
 plots['tran_time']['plot'].x_range = plots['prop_time']['plot'].x_range
 plots['tran_freq']['plot'].x_range = plots['prop_freq']['plot'].x_range
 
-layout = column(row(widgets, row(div, width=200), plot_graph, height=300),
+layout = column(row(widgets, data_table, plot_graph, height=300),
                 row(plots['prop_time']['plot'], plots['prop_freq']['plot'], height=300),
                 row(plots['tran_time']['plot'], plots['tran_freq']['plot'], height=300), sizing_mode='scale_height')
 
 curdoc().add_root(layout)
 curdoc().title = "ASOPE Photonic Circuit Design"
+
+load_graph()
--- interactive/model.py
+++ interactive/model.py
@@ -2,41 +2,53 @@ import networkx as nx
 import random
 import string
 
-class VisualGraph(nx.Graph):
 
-    @classmethod
-    def from_asope_graph(cls, asope_graph):
-        G = cls()
-        # asope_graph = nx.convert_node_labels_to_integers(asope_graph, first_label=0, ordering='default', label_attribute=None)
-        for i, node in enumerate(asope_graph.nodes):
-            G.add_node(node, test_attr=asope_graph.nodes[node]['model'].node_acronym)
-        for j, edge in enumerate(asope_graph.edges):
-            G.add_edge(edge[0], edge[1], test_attr='edge_test_attr')
-        return G
+class AccessGraph(object):
 
-    @classmethod
-    def create_empty_graph(cls):
-        G = cls()
-        n = 2
-        for node in range(0, n):
-            G.add_node(node, test_attr='test')
-        for u, v in zip(range(0,n-1), range(1,n)):
-            G.add_edge(u, v, test_attr='test')
-        return G
+    def __init__(self):
+        return
 
-    def export_as_dict(self):
-        edge_dict = dict(start=[edge[0] for edge in self.edges],
-                         end=[edge[1] for edge in self.edges],
+    # @classmethod
+    # def from_asope_graph(cls, asope_graph):
+    #     G = cls()
+    #     # asope_graph = nx.convert_node_labels_to_integers(asope_graph, first_label=0, ordering='default', label_attribute=None)
+    #     for i, node in enumerate(asope_graph.nodes):
+    #         G.add_node(node, test_attr=asope_graph.nodes[node]['model'].node_acronym)
+    #     for j, edge in enumerate(asope_graph.edges):
+    #         G.add_edge(edge[0], edge[1], test_attr='edge_test_attr')
+    #     return G
+
+    @staticmethod
+    def get_graph_positions(graph):
+        return nx.kamada_kawai_layout(graph)
+
+    @staticmethod
+    def init_dict():
+        edge_dict = dict(start=[],
+                         end=[],
                          )
-        # for attr in ['test_attr']:
-        #     edge_dict[attr] = ['q' for _ in self.edges]
-        #                  test_attr=[self.edges[edge]['test_attr'] for edge in self.edges],
-        #                  )
+        node_dict = dict(index=[],
+                         type=[],
+                         test_attr=[]
+                         )
+        return node_dict, edge_dict
 
-        node_indices = [node for node in self.nodes]
-        node_dict = dict(index=node_indices,
-                         type=[random.choice(string.ascii_lowercase) for _ in self.nodes],
-                         test_attr=[self.nodes[node]['test_attr'] for node in self.nodes]
+    @staticmethod
+    def export_as_dict(graph):
+        edge_dict = dict(start=[edge[0] for edge in graph.edges],
+                         end=[edge[1] for edge in graph.edges],
+                         component=[graph.edges[edge]['model'].node_acronym for edge in graph.edges],
+                         edge=[edge for edge in graph.edges]
+                         )
+
+        node_dict = dict(index=[node for node in graph.nodes],
+                         type=[random.choice(string.ascii_lowercase) for _ in graph.nodes],
+                         component=[graph.nodes[node]['model'].node_acronym for node in graph.nodes]
                          )
 
         return node_dict, edge_dict
+
+    @staticmethod
+    def get_layout(graph):
+        graph_layout = nx.kamada_kawai_layout(graph)
+        return graph_layout
