ee7c88d41cc84b0de8e1ee84ad4c4f6644badcd0
Benjamin MacLellan <ben.mac@live.ca>
2021-02-19 17:29:28 -0500

    minor bug fix in bash script

--- batch_run_scripts.sh
+++ batch_run_scripts.sh
@@ -2,20 +2,16 @@
 
 
 FILES="
-#run_topology_phase_sensitivity.py
-#run_topology_phase_sensitivity.py
-#run_topology_pulse_rep.py
-#run_topology_rfawg.py
 batch_pulse_rep.py
 batch_rfawg.py
 "
 cd scripts
 
-for i in 1 2 3
+for i in 1 2
 do
 	for f in $FILES
 	do
 		echo $f $i
-		python3 $fe --dir 20210219_evolution_tree
+		python3 $f --dir 20210219_evolution_tree
 	done
 done
